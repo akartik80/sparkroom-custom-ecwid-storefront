efacd7215fcaa39d69b671c521a9046ca25db712
akartik80 <akartik80@gmail.com>
2022-01-29 15:49:14 +0530

    trim product card text

---
+++ js/storefront.js
@@ -0,0 +1,20 @@
+let jquery = document.createElement("script");
+jquery.type = "text/javascript";
+jquery.src = "https://code.jquery.com/jquery-3.6.0.min.js";
+document.head.appendChild(s);
+
+let PRODUCT_CARD_MAX_NAME_LENGTH = 40;
+let ellipsis = "...";
+
+// Add ellipsis for long products
+$('.ins-component__title-inner').text((_, oldText) => {
+    console.log("=============");
+    console.log(oldText.length);
+    console.log(PRODUCT_CARD_MAX_NAME_LENGTH + ellipsis.length);
+    console.log("=============");
+    if (oldText.length > PRODUCT_CARD_MAX_NAME_LENGTH + ellipsis.length) {
+        return oldText.slice(0, PRODUCT_CARD_MAX_NAME_LENGTH) + ellipsis;
+    }
+
+    return oldText;
+});
